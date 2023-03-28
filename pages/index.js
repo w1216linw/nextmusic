0a2d4b3c3cf8c4e2bd7a567c2a7a9b525b82ded1
Weiwei <w1216lin@gmail.com>
2023-03-28 00:25:47 -0500

    feat: show liked songs

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import { getSession } from "next-auth/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between h-screen bg-black">
-      hello world
+    <div className="grid place-items-center flex-grow h-screen bg-gradient-to-b from-slate-500 to-slate-900">
+      <h1 className="text-6xl m-auto">Welcome</h1>
     </div>
   );
 }
--- pages/liked.js
+++ pages/liked.js
@@ -1,5 +1,34 @@
+import Song from "@/components/center/Song";
+import useSpotify from "@/hooks/useSpotify";
+import { HeartIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+
 const Like = () => {
-  return <div>Like</div>;
+  const spotify = useSpotify();
+  const [likedSong, setLikedSong] = useState([]);
+  useEffect(() => {
+    if (!spotify) return;
+    spotify.getMySavedTracks().then((res) => setLikedSong(res.body.items));
+  }, [spotify]);
+  return (
+    <div className="flex-grow ">
+      <header className="flex gap-2 items-center h-80 p-5 bg-gradient-to-b from-slate-500 to-black">
+        <HeartIcon className="h-20 text-green-500" />
+        <h1
+          className="text-4xl font-bold text-transparent bg-gradient-to-r from-green-400 to-red-400 bg-clip-text"
+          style={{ lineHeight: "3rem" }}
+        >
+          Liked Songs
+        </h1>
+      </header>
+      <div className="text-white">
+        {likedSong.length >= 1 &&
+          likedSong.map((item) => (
+            <Song key={item.track.id} song={item.track} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Like;
